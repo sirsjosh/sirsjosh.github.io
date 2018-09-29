32e37b5ca4f635cc9dc88eb9e05b4989659d852b
josh <hokojoshua@gmail.com>
2018-09-29 18:01:13 +0800

    nothing serious

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
- $('.port-item').click(function () {
+$('.port-item').click(function () {
       $('.collapse').collapse('hide');
         
     $(document).on('click', '[data-toggle="lightbox"]', function (e) {
